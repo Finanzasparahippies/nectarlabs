388ca24f8d8445c4e32a4871a432c11e5c84e0ab
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2026-07-15 10:58:22 -0700

    fix: course bot 1.8

--- backend/apps/courses/serializers.py
+++ backend/apps/courses/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import ExerciseSubmission
+from .models import ExerciseSubmission, CourseModule
 
 
 class ExerciseSubmissionSerializer(serializers.ModelSerializer):
